79f026f313e94ad2c9486749a88bdcb602966d61
MBlaureNs <42chrono@gmail.com>
2018-02-12 01:00:45 +0000

    game backups

---
+++ lib/memory/game_backup.ex
@@ -0,0 +1,22 @@
+# adapted from nat's hangman repo
+# https://github.com/NatTuck/hangman2/tree/proc-state
+
+defmodule Memory.GameBackup do
+  use Agent
+
+  def start_link do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(name, game) do
+    Agent.update __MODULE__, fn state ->
+      Map.put(state, name, game)
+    end
+  end
+
+  def load(name) do
+    Agent.get __MODULE__, fn state ->
+      Map.get(state, name)
+    end
+  end
+end
--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -8,7 +8,7 @@ defmodule MemoryWeb.GamesChannel do
   
   def join("games:"<>name, payload, socket) do
     if authorized?(payload) do
-      game = Game.new()
+      game = Memory.GameBackup.load(name) || Game.new()
       socket = socket
       |> assign(:game, game)
       |> assign(:name, name)
@@ -20,18 +20,21 @@ defmodule MemoryWeb.GamesChannel do
 
   def handle_in("click", %{"x" => x, "y" => y}, socket) do
     game = Game.click(socket.assigns[:game], x+y*4)
+    Memory.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     {:reply, {:ok, %{"game" => Game.client_view(game)}}, socket}
   end
 
   def handle_in("restart", payload, socket) do
     game = Game.restart(socket.assigns[:game])
+    Memory.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     {:reply, {:ok, %{"game" => Game.client_view(game)}}, socket}
   end
   
   def handle_info(:unlock, socket) do
     game = Game.unlock(socket.assigns[:game])
+    Memory.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     MemoryWeb.Endpoint.broadcast(socket.topic, "update", %{"game" => Game.client_view(game)})
     {:noreply, socket}
